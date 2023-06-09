997bd96df36f85435fd7ad76871e5d478ee6abd3
Anton Blagoev <32401232+AntonBlagoev@users.noreply.github.com>
2023-06-09 18:39:29 +0300

    ASP-NET-Core-MVC-Introduction-Exercises
    MVCIntroDemo, ChatApp & TextSplitterApp

---
+++ ChatApp/ChatApp/Models/Message/ChatViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ChatApp.Models.Message
+{
+    public class ChatViewModel
+    {
+        public ChatViewModel()
+        {
+            this.Messages = new HashSet<MessageViewModel>();
+        }
+        public MessageViewModel CurrentMessage { get; set; } = null!;
+
+        public ICollection<MessageViewModel> Messages { get; set; }
+
+    }
+}
